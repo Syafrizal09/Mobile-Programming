6d3dae669c1c2f0470966e74abdc6c96019ef3f7
Syafrizal09 <80763526+Syafrizal09@users.noreply.github.com>
2021-09-29 22:03:23 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,94 @@
 import 'package:flutter/material.dart';
 
-class AplikasiKu extends StatelessWidget {
-  const AplikasiKu({required this.title, Key? key}) : super(key: key);
-  final Widget title;
+class LayoutRow extends StatelessWidget {
+  const LayoutRow({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.red[500]),
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Menu Navigasi',
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
-        ],
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar( //bar atas aplikasi
+              flexibleSpace: SafeArea( //widget flexsiblespace
+                  // ignore: avoid_unnecessary_containers
+                  child: Container(
+                child: Column( //untuk membuat kolom pertama
+                  children: [
+                    Row(
+                      //untuk membuat baris pertama
+                      children: const [
+                        IconButton(
+                          icon: Icon(Icons.menu), //widget icon
+                          tooltip:
+                              'Navigation menu', //nama untuk iconnya yaitu navigation menu
+                          onPressed:
+                              null, // berisi null yang artinya tidak ada aksi yang akan terjadi jika kita menekannya
+                        ),
+                        Spacer(), //untuk memberi jarak atau spasi //pemisah atas spasi kolom
+                        Text( //widget teks
+                          'Kantin Politeknik Kampar', //kalimat yang akan tampil
+                          textAlign: TextAlign
+                              .center, //menentukan letak teks di tengah
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
+                ),
+              )),
+            // ignore: avoid_unnecessary_containers
+            ),
+            // ignore: avoid_unnecessary_containers
+            body: Container( //bagian body
+              child: Column( //membuat kolom ke dua
+                children: [
+                  Row( //membuat baris ke dua
+                    children: const [
+                      Text(
+                        'kolom Pertama baris Pertama', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .left, //menentukan letak teks di kolom kiri
+                      ),
+                      Spacer(), //pemisah atas spasi kolom
+                      Text(
+                        'kolom kedua baris Pertama', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .center, //menentukan letak teks di kolom  tengah
+                      ),
+                      Spacer(), //pemisah atas spasi kolom
+                      Text(
+                        'kolom Ketiga baris Pertama', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .right, //menentukan letak teks di kolom kanan
+                      ),
+                    ],
+                  ), //penutup baris pertama
+                  Row(
+                    children: const [
+                      Text(
+                        'kolom Pertama baris kedua', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .left, //menentukan letak teks di kolom kiri
+                      ),
+                      Spacer(), //pemisah atas spasi kolom
+                      Text(
+                        'kolom kedua baris kedua', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .center, //menentukan letak teks di kolom  tengah
+                      ),
+                      Spacer(), //pemisah atas spasi kolom
+                      Text(
+                        'kolom ketiga baris kedua', //kalimat yang akan tampil
+                        textAlign: TextAlign
+                            .right, //menentukan letak teks di kolom kanan
+                      ),
+                    ],
+                  ),
+                ],
+              ), //batas pembuatan kolom
+            )));
   }
 }
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          AplikasiKu(
-            title: Text(
-              'Menu',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hallo Dunia'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'AplikasiKu',
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
+  // ignore: prefer_const_constructors
+  runApp(LayoutRow());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:AplikasiKu/main.dart';
+// import 'package:aplikasi/main.dart';
 
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
